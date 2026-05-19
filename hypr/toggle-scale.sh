63a58be8b9adab45050300109015a79eb6499711
abhinavmishra094 <abhinavmishra094@users.noreply.github.com>
2026-05-19 07:53:52 +0530

    Set HDR luminance from monitor EDID

--- hypr/toggle-scale.sh
+++ hypr/toggle-scale.sh
@@ -10,5 +10,5 @@ if [[ "$CURRENT_FORMAT" == "XRGB2101010" ]]; then
     hyprctl keyword monitor "$MONITOR,highrr,0x0,1.0,vrr,3"
 else
     echo "Enabling HDR..."
-    hyprctl keyword monitor "$MONITOR,highrr,0x0,1.0,bitdepth,10,cm,hdr,sdrbrightness, 1.1, sdrsaturation, 1.3,vrr,3"
+    hyprctl keyword monitor "$MONITOR,highrr,0x0,1.0,bitdepth,10,cm,hdr,sdrbrightness,0.5,sdrsaturation,1.0,sdr_min_luminance,0.0,sdr_max_luminance,456,min_luminance,0.0,max_luminance,456,max_avg_luminance,254,vrr,3"
 fi
